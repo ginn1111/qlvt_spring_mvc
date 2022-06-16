b08e1e853845c62cd63ab719525be06a728eac11
= <=>
2022-06-16 21:54:14 +0700

    add

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,37 +1,25 @@
 package controller;
 
-import entity.Account;
+import entity.NhanVien;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import service.TestService;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+
 @Controller
+@Transactional
 public class TestController {
     @Autowired
-    SessionFactory ss;
-
-    @Autowired
-    TestService testService;
-
-    @Transactional
+    SessionFactory sessionFactory;
     @RequestMapping("test")
     public String test(ModelMap model) {
-        List<Account> listAccount = ss.getCurrentSession().createQuery("FROM Account AS A").list();
-
-        for (Account account:
-             listAccount) {
-            System.out.println(account.getIdRole() + " " + account.getPassword());
-        }
-
-        testService.testService();
-
-        model.addAttribute("tests", listAccount);
+        List<NhanVien> list = (List<NhanVien>) sessionFactory.getCurrentSession().createQuery("FROM NhanVien").list();
+        System.out.println(list.size());
 
         return "test";
     }
